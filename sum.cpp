750a4f9b489d794b316ff182dbaeb6bafe0cec77
Sonu Singh <ss8137794@gmail.com>
2025-09-11 14:09:47 +0530

    input two number sum

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2,sum;
+    cout<<"enter tha first number:"<<endl;
+    cin>>num1;
+    cout<<"num1 value is"<<num1<<endl;
+    cout<<"enter tha second number:"<<endl;
+    cin>>num2;
+    cout<<"num2 value is"<<num2<<endl;
+    sum = num1+num2;
+    cout<<"tha sum is:"<<sum<<endl;
+}
