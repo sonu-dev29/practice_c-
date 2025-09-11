66ebe9c2599293a5978e566141735d4dad5797b1
Sonu Singh <ss8137794@gmail.com>
2025-09-11 15:07:08 +0530

    tha difference

---
+++ .vscode/difference.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"enter tha first number"<<endl;
+    cin>>num1;
+    cout<<"first number"<<num1<<endl;
+    cout<<"enter the second number"<<endl;
+    cin>>num2;
+    cout<<"second number"<<num2<<endl;
+    int difference = num1-num2;
+    cout<<"tha difference is"<<difference<<endl;
+}
