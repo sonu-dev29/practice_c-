560821a59344283924f8949d5f82e31e64cad32c
Sonu Singh <ss8137794@gmail.com>
2025-09-11 22:04:41 +0530

    product

---
+++ .vscode/product.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b;
+    cout<<"enter tha a"<<endl;
+    cin>>a;
+    cout<<"a:"<<a<<endl;
+    cout<<"enter ths b"<<endl;
+    cin>>b;
+    cout<<"b:"<<b<<endl;
+    float sum  =a+b;
+    cout<<"tha of value:"<<sum<<endl;
+}
+
