741ea44a005b074a19913e0bdaa8a4c73a69ec33
Sonu Singh <ss8137794@gmail.com>
2025-09-12 11:09:24 +0530

    three sum

---
+++ three.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number1,number2,number3;
+    cout<<"enter tha first number"<<endl;
+    cin>>number1;
+    cout<<"first number:"<<number1<<endl;
+    cout<<"enter tha second number"<<endl;
+    cin>>number2;
+    cout<<"second number:"<<number2<<endl;
+    cout<<"enter tha third number"<<endl;
+    cin>>number3;
+    cout<<"third number:"<<number3<<endl;
+   int sum = number1+number2+number3;
+    cout<<"tha value = "<<sum<<endl;
+}
