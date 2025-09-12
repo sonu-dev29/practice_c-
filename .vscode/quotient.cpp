ed3c412b050ce80ea2f66a88b62d312d910be5fc
Sonu Singh <ss8137794@gmail.com>
2025-09-12 08:56:41 +0530

    multiplies

---
+++ .vscode/quotient.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int number1,number2,multiplies;
+cout<<"enter tha first number"<<endl;
+ cin>>number1;
+cout<<"first:"<<number1<<endl;
+cout<<"enter tha second number"<<endl;
+cin>>number2;
+cout<<" second:"<<number2<<endl;
+multiplies = number1*number2;
+cout<<"sis tha:"<<multiplies<<endl;
+}
