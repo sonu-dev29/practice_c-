9a53c6e9bf3f79535dc83fa981242fcfde87a284
Sonu Singh <ss8137794@gmail.com>
2025-09-12 10:26:36 +0530

    remainder

---
+++ remainder.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number1,number2,remainder;
+    cout<<"enter tha first number"<<endl;
+    cin>>number1;
+    cout<<"first number:"<<number1<<endl;
+    cout<<"enter tha second number"<<endl;
+    cin>>number2;
+    cout<<"second number:"<<number2<<endl;
+    remainder = number1,number2;
+    cout<<"tha value ="<<remainder<<endl;
+}
