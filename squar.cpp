c31641f85ab00d8ea31919828bc47da64d7c5fca
Sonu Singh <ss8137794@gmail.com>
2025-09-12 20:18:58 +0530

    squar

---
+++ squar.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number1,squar;
+    cout<<"enter tha number"<<endl;
+    cin>>number1;
+    cout<<"tha number:"<<number1<<endl;
+    squar = number1;
+    cout<<"tha final value ="<<squar<<endl;
+}
