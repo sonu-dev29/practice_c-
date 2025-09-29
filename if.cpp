43b8270c5b92959031a2a85e71ed78108929ec14
Sonu Singh <ss8137794@gmail.com>
2025-09-29 15:21:28 +0530

    if rule

---
+++ if.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int age;
+cout<<"enter your age"<<endl;
+cin>>age;
+if(age> 18){
+    cout<<"you can vot";
+}
+else{
+    cout<<"you cannot vot";
+}
+
+return 0;
+}
