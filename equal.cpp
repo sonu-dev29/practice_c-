5f5fba2cf47aea075ca85e6376c1ae55a46bf1e1
Sonu Singh <ss8137794@gmail.com>
2025-10-01 17:31:48 +0530

    equal

---
+++ equal.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int b=3,c=4;
+    if(3==4){
+        cout<<"three equal to four"<<endl;
+
+    }
+    else{
+        cout<<"three and four not equal"<<endl;
+    }
+}
