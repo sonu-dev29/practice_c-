c06a0011309ba7bf2e1ad73b1fe3e9bf325e9e1f
Sonu Singh <ss8137794@gmail.com>
2025-10-01 17:10:10 +0530

    even number

---
+++ condition.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=18,b=2;
+    if(2/18==0){
+        cout<<"this is a even number"<<endl;
+    }
+    else{
+        cout<<"this is odd number"<<endl;
+    }
+    return 0;
+}
